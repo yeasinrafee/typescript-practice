2b2c94f4c0746c04b70b6358d20560b2a94c36c8
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-08-25 22:07:19 +0600

    Literal Type in ts

---
+++ dist/literal_type.js
@@ -0,0 +1,4 @@
+"use strict";
+let quantity = 100;
+console.log(quantity);
+//# sourceMappingURL=literal_type.js.map
---
+++ src/literal_type.ts
@@ -0,0 +1,6 @@
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = "cm" | "inch";
+
+console.log(quantity);
