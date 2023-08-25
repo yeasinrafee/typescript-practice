e6301aff1701369733e573fe08a8add9bb7bbf93
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-08-25 22:32:16 +0600

    Optional chainning in ts

---
+++ dist/optional_chaining.js
@@ -0,0 +1,8 @@
+"use strict";
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+//# sourceMappingURL=optional_chaining.js.map
---
+++ src/optional_chaining.ts
@@ -0,0 +1,20 @@
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+
+// Optional Property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator 
+// customer?.[0]
+
+// Optional Call
+let log: any = null;
+log('a')        // It'll give an error;
+log?('a');
